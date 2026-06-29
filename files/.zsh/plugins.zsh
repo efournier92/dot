8e4256f81905efa2a8b40d92249f8bcb1d73ed57
efournier92 <efournier92@gmail.com>
2026-06-28 21:03:49 -0400

    Update Plugin-Install Mechanism

---
+++ files/.zsh/plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( "$HOME/.zsh/plugins/github.com/Aloxaf/fzf-tab" )
+source "$HOME/.zsh/plugins/github.com/Aloxaf/fzf-tab/fzf-tab.plugin.zsh"
+fpath+=( "$HOME/.zsh/plugins/github.com/zsh-users/zsh-syntax-highlighting" )
+source "$HOME/.zsh/plugins/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
